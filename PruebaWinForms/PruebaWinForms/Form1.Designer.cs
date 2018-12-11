5dd322c9350254e9ddefbbc3e25a66878a0e93b7
Juan Agudo <juanantonioagudo@gmail.com>
2018-12-11 01:48:29 +0100

    Quinto Commit Ejemplo de carga de fichero en GridView

--- PruebaWinForms/PruebaWinForms/Form1.Designer.cs
+++ PruebaWinForms/PruebaWinForms/Form1.Designer.cs
@@ -36,6 +36,9 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -61,7 +64,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(115, 45);
+            this.label2.Location = new System.Drawing.Point(53, 45);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(95, 20);
             this.label2.TabIndex = 2;
@@ -71,7 +74,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(95, 92);
+            this.label3.Location = new System.Drawing.Point(33, 92);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(115, 20);
             this.label3.TabIndex = 3;
@@ -79,21 +82,21 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(238, 39);
+            this.textBox1.Location = new System.Drawing.Point(176, 39);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 26);
             this.textBox1.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(238, 85);
+            this.textBox2.Location = new System.Drawing.Point(176, 85);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 26);
             this.textBox2.TabIndex = 5;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(238, 130);
+            this.button2.Location = new System.Drawing.Point(176, 130);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 30);
             this.button2.TabIndex = 6;
@@ -104,17 +107,31 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(256, 185);
+            this.label4.Location = new System.Drawing.Point(194, 185);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(82, 20);
             this.label4.TabIndex = 7;
             this.label4.Text = "Resultado";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(314, 39);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(461, 298);
+            this.dataGridView1.TabIndex = 8;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
@@ -125,6 +142,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -140,6 +158,8 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- PruebaWinForms/PruebaWinForms/Form1.cs
+++ PruebaWinForms/PruebaWinForms/Form1.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace PruebaWinForms
 {
     public partial class Form1 : Form
     {
+        public string ARCHIVO = "";
+
         public Form1()
         {
             InitializeComponent();
@@ -24,7 +27,8 @@ namespace PruebaWinForms
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            //Application.Exit();
+            cargarArchivo();
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -48,5 +52,66 @@ namespace PruebaWinForms
                 label4.Text = "Parece que no has metido 2 números...";
             }
         }   
+        public void LeerArchivo(DataGridView tabla, char caracter, string ruta)
+        {
+            StreamReader objReader = new StreamReader(ruta);
+            string sLine = "";
+            int fila = 0;
+
+            tabla.Rows.Clear();
+            tabla.AllowUserToAddRows = false;
+
+            do
+            {
+                sLine = objReader.ReadLine();
+                if (sLine != null)
+                {
+                    if (fila == 0)
+                    {
+                        tabla.ColumnCount = sLine.Split(caracter).Length;
+                        nombrarTitulos(tabla, sLine.Split(caracter));
+                        fila += 1;
+                    }
+                    else
+                    {
+                        agregarFila(tabla, sLine, caracter, fila);
+                        fila += 1;
+                    }
+                }
+            } while (!(sLine == null));
+            objReader.Close();
+        }
+
+        public static void nombrarTitulos(DataGridView tabla, string[] titulos)
+        {
+            int x = 0;
+            for (x = 0; x < tabla.ColumnCount; x++)
+            {
+                tabla.Columns[x].HeaderText = titulos[x];
+            }
+        }
+
+        public static void agregarFila(DataGridView tabla, string linea, char caracter, int fila)
+        {
+            string[] arreglo = linea.Split(caracter);
+            tabla.Rows.Add(arreglo);
+        }
+
+        public void cargarArchivo()
+        {
+            try
+            {
+                this.openFileDialog1.ShowDialog();
+                if(!string.IsNullOrEmpty(this.openFileDialog1.FileName))
+                {
+                    ARCHIVO = this.openFileDialog1.FileName;
+                    LeerArchivo(dataGridView1, ',', ARCHIVO);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
     }
 }
